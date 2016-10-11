d4f004f78b2ce986e1adee70fcc13c76d5e7cb55
Jason's iMac <1348748408@qq.com>
2016-10-11 09:26:55 +0800

    a

---
+++ TestPod/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestPod
+//
+//  Created by Jason on 2016/10/11.
+//  Copyright © 2016年 Jason. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
